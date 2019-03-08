b2f019e9f1d0212b23efb3a18eba19fb0ff72b47
unknown <1469406888@mil>
2019-03-08 13:54:25 -0800

    edit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,33 +18,36 @@ resetGuesses();
 document.addEventListener('keyup', userInput);
 function userInput(event) {
 	var userGuess = event.key.toLowerCase();
-		
-	if (hangmanLetters.indexOf("_") > -1 && guessesLeft > 1) { // word not complete and guesses left
-		if (lettersWord.indexOf(userGuess) === -1 && lettersGuessed.indexOf(userGuess) === -1) { //if userGuess not in word and not in lettersGuessed
-			guessesLeft--;
-			guessesLeftTxt.textContent = "Number of guesses remaining: " + guessesLeft;
-			lettersGuessed.push(userGuess);
-			guessesSoFarTxt.textContent = "Letters already guessed: " + lettersGuessed.join(" ");
-		} else if (lettersWord.indexOf(userGuess) === -1 && lettersGuessed.indexOf(userGuess) > -1) { //if userGuess not in word and already in lettersGuessed
+	var key = event.keyCode;
+	
+	if (key >= 65 && key <= 90) {
+		if (hangmanLetters.indexOf("_") > -1 && guessesLeft > 1) { // word not complete and guesses left
+			if (lettersWord.indexOf(userGuess) === -1 && lettersGuessed.indexOf(userGuess) === -1) { //if userGuess not in word and not in lettersGuessed
+				guessesLeft--;
+				guessesLeftTxt.textContent = "Number of guesses remaining: " + guessesLeft;
+				lettersGuessed.push(userGuess);
+				guessesSoFarTxt.textContent = "Letters already guessed: " + lettersGuessed.join(" ");
+			} else if (lettersWord.indexOf(userGuess) === -1 && lettersGuessed.indexOf(userGuess) > -1) { //if userGuess not in word and already in lettersGuessed
 
-		} else if (lettersWord.indexOf(userGuess) > -1) { // userGuess in word
-			for (var i = 0; i < lettersWord.length; i++) {
-				if (lettersWord[i] === userGuess) {
-					hangmanLetters[i]=userGuess;
-					//console.log(hangmanLetters);
-					wordTxt.textContent = "Current word: " + hangmanLetters.join(" ");
+			} else if (lettersWord.indexOf(userGuess) > -1) { // userGuess in word
+				for (var i = 0; i < lettersWord.length; i++) {
+					if (lettersWord[i] === userGuess) {
+						hangmanLetters[i]=userGuess;
+						//console.log(hangmanLetters);
+						wordTxt.textContent = "Current word: " + hangmanLetters.join(" ");
+					}
 				}
+				if (hangmanLetters.indexOf("_") === -1) { // if word completed
+					wins++;
+					winsTxt.textContent = "Wins: " + wins;
+					answerTxt.textContent = "Score! It's " + currentWord.toUpperCase() + " with the finish!!!";
+					resetGuesses();
+				} 
 			}
-			if (hangmanLetters.indexOf("_") === -1) { // if word completed
-				wins++;
-				winsTxt.textContent = "Wins: " + wins;
-				answerTxt.textContent = "Score! It's " + currentWord.toUpperCase() + " with the finish!!!";
-				resetGuesses();
-			} 
+		} else if (hangmanLetters.indexOf("_") > -1 && guessesLeft === 1) { // word not complete and guesses run out
+			answerTxt.textContent = "So close " + currentWord.toUpperCase() + ". :( Better luck next time!";
+			resetGuesses();
 		}
-	} else if (hangmanLetters.indexOf("_") > -1 && guessesLeft === 1) { // word not complete and guesses run out
-		answerTxt.textContent = "So close " + currentWord.toUpperCase() + ". :( Better luck next time!";
-		resetGuesses();
 	}
 }
 
@@ -57,7 +60,7 @@ function resetGuesses() {
 	guessesSoFarTxt.textContent = "Letters already guessed: ";
 
 	currentWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
-	//console.log(currentWord);
+	console.log(currentWord);
 	lettersWord = currentWord.split('');
 	for (var i = 0; i < lettersWord.length; i++) {
 		hangmanLetters.push("_")
